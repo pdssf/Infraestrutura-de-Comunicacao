7abf512186445c721311d0a15bbd28dd7eb08d20
Vitor <vitorgs2@cin.ufpe.br>
2018-05-30 18:25:36 -0300

    Download and upload are now working.

--- Questao 2/Client/client.py
+++ Questao 2/Client/client.py
@@ -10,15 +10,18 @@ clientSocket.connect((serverName,serverPort))
 commandLine = ''
 
 #asks for the user name
-#initial exchange
+#initial exchange, login
 userName = input( 'Please, type your username:' )
 clientSocket.send(userName.encode())
 while(commandLine != 'exit'):
     commandLine = input( 'Please, type your command: GET, PUT, clientSend or exit \n' )
     clientSocket.send(commandLine.encode())
+    #sends command to the server and wait a response
     print('Waiting for command line')
     serverResponse = clientSocket.recv(1024)
     print('Resposta do servidor', serverResponse.decode())
+
+    #sends file to server
     if(serverResponse.decode() == 'helo send me file'):
         #digita nome do arquivo
         fileName = input('Digite o nome do arquivo a ser enviado: \n')
@@ -33,10 +36,22 @@ while(commandLine != 'exit'):
         print('sended file')
         #clientSocket.shutdown(clientSocket.SHUT_WR)
 
-
-    if(serverResponse.decode() == 'GET'):
+    #receives file from server
+    elif(serverResponse.decode() == 'GET'):
         print('Client received GET back')
+        fileName = input('What file would you like to download?\n')
+        clientSocket.send(fileName.encode())#1
+        receivingFile = open(fileName,'wb')
+        #receivingPart = clientSocket.recv(1024)#2
+        while(1):
+            receivingPart = clientSocket.recv(1024)
+            receivingFile.write(receivingPart)
+            if len(receivingPart) < 1024:
+                break
+        receivingFile.close()
+        print('received file')
 
+    #to be implemented
     elif(serverResponse.decode() == 'PUT'):
         print('Client received PUT back')
 
--- Questao 2/Server/server.py
+++ Questao 2/Server/server.py
@@ -23,20 +23,23 @@ print ('The server is ready to receive')
 while 1:
 	connectionSocket, addr = serverSocket.accept()
 	userName = connectionSocket.recv(1024)
+	#login
 	result = checkUserName(userName)
 	while(commandLine != 'exit'):
+		#menu
 		print('Waiting for command line from client')
 		commandLine = connectionSocket.recv(1024)
 		print('Received. Client response: ',commandLine.decode())
 
 		#possible user commands:
+		#receives file from client
 		if(commandLine.decode() == 'clientSend'):
 			connectionSocket.send('helo send me file'.encode())
-			#recebe fileName
+			#receives fileName
 			fileName = connectionSocket.recv(1024)
 			fileName = fileName.decode()
 			print('received fileName:', fileName)
-			#concatena 'username' '/' 'fileName'
+			#concatenate 'username' '/' 'fileName'
 			path = userName.decode() + '/' + fileName
 			receivingFile = open(path,'wb')
 			#starts to receive the file
@@ -49,10 +52,24 @@ while 1:
 			receivingFile.close()
 			print('received file')
 
-
+		#sends file to client
 		elif(commandLine.decode() == 'GET'):
 			connectionSocket.send(commandLine)
+			print('waiting for file name')
+			fileName = connectionSocket.recv(1024)#1
+			fileName = fileName.decode()
+			print('received file name')
+			path = userName.decode() + '/' + fileName
+			sendingFile = open(path,'rb')
+			sendingPart = sendingFile.read(1024)
+			while (sendingPart):
+				connectionSocket.send(sendingPart)
+				sendingPart = sendingFile.read(1024)
+			sendingFile.close()
+			print('sended file')
 
+
+		#to be implemented
 		elif(commandLine.decode() == 'PUT'):
 			connectionSocket.send(commandLine)
 
