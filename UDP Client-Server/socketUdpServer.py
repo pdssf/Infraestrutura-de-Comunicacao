f851f4a2fcccac703292c2cc9787c817a5d1336a
Paulo Salgado <pdssf@cin.ufpe.br>
2018-06-25 09:24:15 -0300

    coments on archives

--- UDP Client-Server/socketUdpServer.py
+++ UDP Client-Server/socketUdpServer.py
@@ -8,7 +8,14 @@ serverSocket.bind(('', serverPort))
 print ('servidor criado!\n')
 
 while 1:
+	#aguarda requisição do cliente
     message, clientAddress = serverSocket.recvfrom(2048)
+
+    #imprime mensagem decodificada
     print(message.decode('UTF-8','strict'))
+
+    #capitaliza letras da menssagem
     modifiedMessage = message.upper()
+
+    #envia mensagem capitalizada 
     serverSocket.sendto(modifiedMessage, clientAddress)
